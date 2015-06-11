2569342fc959a46bd5f8fa967d752efa123fbf9c
zonoise <zonoise@gmail.com>
2015-06-11 09:33:37 +0900

    create Post model

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -3,11 +3,24 @@
 namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BlogBundle\Entity\PostRepository;
+use BlogBundle\Entity\Post;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name=null)
     {
+        $post = new Post();
+        $post->setTitle("aaa");
+        $post->setBody("aaa");
+        $post->setCreatedAt(new \DateTime());
+        $post->setUpdatedAt(new \DateTime());
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $repo = $em->getRepository("BlogBundle:Post");
+        $em->persist($post);
+        $em->flush();
+        #$repository->find();
         return $this->render('BlogBundle:Default:index.html.twig', array('name' => $name));
     }
 }
